d1bd05251e57869b659bdb8a667d33ed7ffe18fb
Irina Dolgova <claycharm@yandex.ru>
2022-08-30 19:14:39 +0300

    add tests

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,36 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.patch;
+import static org.hamcrest.Matcher.*;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                //.header("Content-type", "application/json; charset=UTF-8");
+                .contentType(ContentType.JSON)
+                //  .body("", hasSize(3))
+//                .body("[1].currency",equalTo("USD"))
+//                .body("")
+//                .body("[0].balance",greaterThanOrEqualTo(0))
+//                .body("every {it.balance >= 0}",is(true))
+//                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+
+        ;
     }
 }
